6ec10e0b89798c6ccb27065b5da80a40553dce8e
jeffyboyse <bestjeff03@gmail.com>
2025-11-03 14:54:26 +0100

    heheha

--- Coregameplay/enemies.js
+++ Coregameplay/enemies.js
@@ -1,21 +1,26 @@
+// Definierar en klass för fiender
 class enemies {
+    // Konstruktor för att skapa en fiende med namn, hälsa, skada, hastighet och position/storlek
     constructor(name, health, damage, speed, x , y, w, h) {
-        this.name = name;
-        this.health = health;
-        this.damage = damage;
-        this.speed = speed;
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-
+        this.name = name;     // Fiendens namn
+        this.health = health; // Fiendens hälsa
+        this.damage = damage; // Skada som fienden gör
+        this.speed = speed;   // Fiendens hastighet
+        this.x = x;           // Fiendens x-position
+        this.y = y;           // Fiendens y-position
+        this.w = w;           // Fiendens bredd
+        this.h = h;           // Fiendens höjd
     }
 }
 
+// Definierar en klass för geten som är en typ av fiende
 class goat extends enemies {
+    // Konstruktor för att skapa en get
     constructor() {
+        // Anropar basklassens konstruktor med specifika värden för geten
         super("Goat", 50, 10, 5,);
+        // Skapar en bild för geten
         this.img = new Image();
-        this.img.src = "goat.png";
+        this.img.src = "goat.png"; // Sätter bildens källa till "goat.png"
     }
 }
