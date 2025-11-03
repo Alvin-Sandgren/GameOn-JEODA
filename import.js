3307ffdcf69338ff68166d53caaa61ac8b212257
jeffyboyse <bestjeff03@gmail.com>
2025-11-03 14:22:19 +0100

    import.js

---
+++ import.js
@@ -0,0 +1,11 @@
+import './Coregameplay/enemies.js';
+import './Coregameplay/combat.js';
+import './Coregameplay/rörelse.js';
+
+
+import './classer.js';
+import './ljud.js';
+import './logik.js';
+import './map.js';
+import './progression.js';
+import './UI.js';
