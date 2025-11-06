2441d3b05041e953857632750292ffbe959be20a
Alvin <alvin.sandgren@elev.ga.ntig.se>
2025-11-06 14:22:15 +0100

    Jeffery

--- Javascript/Coregameplay/ljud.js
+++ Javascript/Coregameplay/ljud.js
@@ -1,11 +1,38 @@
-if (gamestate === "menu") {
-    Menu_music.play();
-}
-else if (gamestate === "Overworld") {
-    Combat_music.pause();
-    Background_music.play();
-}
-else if (gamestate === "combat") {
-    Background_music.pause();
-    Combat_music.play();
+class LjudManager {
+    playMenu() {
+        this.menuMusic = new Audio("Ljud/Menumusic.wav");
+        this.menuMusic.loop = true;
+        this.backgroundMusic = new Audio("Ljud/Background-music.wav");
+        this.backgroundMusic.loop = true;
+        this.combatMusic = new Audio("Ljud/cOMBATMUSIK.wav");
+        this.combatMusic.loop = true;
+        this.backgroundMusic.pause();
+        this.combatMusic.pause();
+        this.menuMusic.play();
+        }
+    
+
+    playOverworld() {
+        this.menuMusic = new Audio("Ljud/Menumusic.wav");
+        this.menuMusic.loop = true;
+        this.backgroundMusic = new Audio("Ljud/Background-music.wav");
+        this.backgroundMusic.loop = true;
+        this.combatMusic = new Audio("Ljud/cOMBATMUSIK.wav");
+        this.combatMusic.loop = true;
+        this.menuMusic.pause();
+        this.combatMusic.pause();
+        this.backgroundMusic.play();
+    }
+
+    playCombat() {
+        this.menuMusic = new Audio("Ljud/Menumusic.wav");
+        this.menuMusic.loop = true;
+        this.backgroundMusic = new Audio("Ljud/Background-music.wav");
+        this.backgroundMusic.loop = true;
+        this.combatMusic = new Audio("Ljud/cOMBATMUSIK.wav");
+        this.combatMusic.loop = true;
+        this.menuMusic.pause();
+        this.backgroundMusic.pause();
+        this.combatMusic.play();
+    }
 }
